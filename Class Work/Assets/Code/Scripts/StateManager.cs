2ee4d1914513737c83dd0779b0b0725f8ed931c2
Brian Foster <brianisbrilliant@gmail.com>
2015-09-22 18:34:33 -0500

    adding cool stuff

--- Class Work/Assets/Code/Scripts/StateManager.cs
+++ Class Work/Assets/Code/Scripts/StateManager.cs
@@ -4,10 +4,12 @@ using Assets.Code.States;
 public class StateManager : MonoBehaviour {
 
 	private BeginState activeState;
+	private BeginState secondState;
 
 	// Use this for initialization
 	void Start () {
-		activeState = new BeginState();
+		activeState = new BeginState("Brian", 26, "professor");
+		secondState = new BeginState("Dumbledore", 150, "Headmaster");
 		Debug.Log("blah blah blah");
 	}
 	
--- Class Work/Assets/Code/States/BeginState.cs
+++ Class Work/Assets/Code/States/BeginState.cs
@@ -5,8 +5,12 @@ namespace Assets.Code.States
 {
 	public class BeginState : IStateBase
 	{
-		public BeginState() {
+		public BeginState(string playerName, int age, string profession) {
 			Debug.Log("Constructing BeginState.");
+			int playerHealth;
+			Debug.Log(playerName);
+			Debug.Log(age);
+			Debug.Log(profession);
 		}
 
 		public void StateUpdate() {
