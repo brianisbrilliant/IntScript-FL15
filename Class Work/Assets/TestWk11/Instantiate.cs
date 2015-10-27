424068066972781e27b7b486fe73cb54c0b8a075
Brian Foster <brianisbrilliant@gmail.com>
2015-10-27 18:06:57 -0500

    wk11 Over the Stack

---
+++ Class Work/Assets/TestWk11/Instantiate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiate : MonoBehaviour {
+
+	public GameObject bullet;
+
+	void FixedUpdate () {
+		if(Input.GetButton("Fire1")) {
+			Shoot();
+		}
+	}
+
+	void Shoot() { 
+		Rigidbody clone;
+        clone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+        clone.velocity = transform.TransformDirection(Vector3.forward * 100);
+	}
+}
