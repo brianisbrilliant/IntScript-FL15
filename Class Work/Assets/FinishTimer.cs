39330b548b203e1e3fa3fe68ecd184927cb90ac7
Brian F <brianisbrilliant@gmail.com>
2015-11-10 11:43:58 -0600

    Added timer to the racing scene.

--- Class Work/Assets/FinishTimer.cs
+++ Class Work/Assets/FinishTimer.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class FinishTimer : MonoBehaviour {
 
+	public float timer;
+			int displayTimer;
+			bool finished = false; 
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,16 @@ public class FinishTimer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(!finished)
+			timer += Time.deltaTime;
+	}
+
+	void OnGUI() {
+		if(GUI.Button(new Rect(10, 10, 80, 30), timer.ToString("0.00")))
+			Application.LoadLevel("racing");			
+	}
+
+	void OnTriggerEnter() {
+		finished = true;
 	}
 }
